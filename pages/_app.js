c2f3bb84da94c5ce46a25d604939e1e589fc6447
nadia77-a <nadiaarapi@hotmail.com>
2023-03-27 12:19:23 +0200

    Updates

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css';
-import '../styles/animate.css';
-import '../styles/font-awesome.min.css';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
